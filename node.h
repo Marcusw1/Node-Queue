0dec5bed588b8ec8e96eb90303407ca02e715429
Marcusw1 <Marcusw1@mail.sfsu.edu>
2015-12-05 14:23:55 -0800

    Create node.h

---
+++ node.h
@@ -0,0 +1,21 @@
+
+#include <cstdlib>
+
+template <class T>
+class Node {
+	
+  public:
+  
+    Node (T data) {
+     next = NULL;
+     value = data;	
+	}
+    
+    Node <T> *next;
+    T value;
+};
+
+
+
+
+
